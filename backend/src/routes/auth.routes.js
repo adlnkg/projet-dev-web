c809eb5eb3967abf419a516a4b08320a6125f57f
adlnkg <adlene.koroghli@gmail.com>
2026-04-21 14:36:32 +0200

    adding verfiy-otp route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, register, verifyOTP } from "../controllers/auth.controller.js";
 
 const router = new express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/verify-otp", verifyOTP);
 
 export default router;

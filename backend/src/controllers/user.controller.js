8a6605bc316c53dbdeec47ada0d7fb960cad4a0e
Po Co <polniolino@gmail.com>
2026-04-29 19:15:31 +0200

    Fix MR

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -84,9 +84,9 @@ export const updateUser = async (req, res) => {
       birthDate,
       sex,
       memberType,
-      avatarUrl,
     } =
       req.body;
+    const avatarUrl = req.uploadedImageUrl;
 
     let parsedBirthDate;
     try {
